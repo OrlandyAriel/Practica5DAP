5bd331da53f941553b4379859b3c4fb440a210a7
Orlandy Ariel <alu0100773408@ull.edu.es>
2016-11-01 18:02:22 +0000

    Redefinición del método getFecha,devuelve objeto Calendar

--- src/ull/patrones/estrategia/IEvento.java
+++ src/ull/patrones/estrategia/IEvento.java
@@ -1,7 +1,10 @@
 package ull.patrones.estrategia;
 
+import java.util.Calendar;
+
 public interface IEvento extends Runnable
 {
 	public long getIdTipoEvento();
-	public String getFecha();
+	public Calendar getFecha();
+	public void start();
 }
