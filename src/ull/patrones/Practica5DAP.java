05ef122013b0e3d5d0f0242637b337a0b267a215
Orlandy Ariel <alu0100773408@ull.edu.es>
2016-11-01 19:38:08 +0000

    main para grafico-main para prueba del patrón

--- src/ull/patrones/Practica5DAP.java
+++ src/ull/patrones/Practica5DAP.java
@@ -1,32 +1,64 @@
 package ull.patrones;
 
+import ull.patrones.auxilares.Fecha;
 import ull.patrones.estrategia.Ev_atasco;
+import ull.patrones.estrategia.Ev_colision;
+import ull.patrones.estrategia.Ev_emergencia;
+import ull.patrones.estrategia.Ev_lentitud;
+import ull.patrones.estrategia.Ev_multa;
 import ull.patrones.singleton.ColaSingleton;
+import ull.patrones.ui.VentanaPrincipal;
 
 public class Practica5DAP
 {
 	public static void main(String[] args)
 	{
-		ColaSingleton a = ColaSingleton.getInstancia();
-		System.out.println(a.toString());
+		new VentanaPrincipal();
+		/**ColaSingleton a = ColaSingleton.getInstancia();
 		
-		a.acolar(new Ev_atasco());
-		System.out.println(a.toString());
+		a.acolar(new Ev_colision());
 		
-		a.acolar(new Ev_atasco());
-		System.out.println(a.toString());
-
-		a.acolar(new Ev_atasco());
-		System.out.println(a.toString());
-
-		a.acolar(new Ev_atasco());
-		System.out.println(a.toString());
-
-		a.acolar(new Ev_atasco());
-		System.out.println(a.toString());
-		a = ColaSingleton.getInstancia();
-		
-		a.desacolar();
-		System.out.println(a.toString());
+		a.acolar(new Ev_multa());
+		try
+		{
+			Thread.sleep(1000);
+		} catch (InterruptedException e)
+		{
+			e.printStackTrace();
+		}
+		a.acolar(new Ev_emergencia());
+		try
+		{
+			Thread.sleep(2000);
+		} catch (InterruptedException e)
+		{
+			e.printStackTrace();
+		}
+		a.acolar(new Ev_atasco() );
+		try
+		{
+			Thread.sleep(2000);
+		} catch (InterruptedException e)
+		{
+			e.printStackTrace();
+		}
+		a.acolar(new Ev_lentitud());
+		try
+		{
+			Thread.sleep(1000);
+		} catch (InterruptedException e)
+		{
+			e.printStackTrace();
+		}
+		while(true){
+			System.out.println(a);
+			try
+			{
+				Thread.sleep(500);
+			} catch (InterruptedException e)
+			{
+				e.printStackTrace();
+			}
+		}*/
 	}
 }
