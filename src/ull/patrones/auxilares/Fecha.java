98c5e127a9dd98bd3053097971a8d46710b626af
Orlandy Ariel <alu0100773408@ull.edu.es>
2016-11-02 17:37:54 +0000

    Cambia el modo de que muestra la fecha

--- src/ull/patrones/auxilares/Fecha.java
+++ src/ull/patrones/auxilares/Fecha.java
@@ -1,32 +1,37 @@
 package ull.patrones.auxilares;
 
 import java.util.Calendar;
-import java.util.GregorianCalendar;
 
 public class Fecha
 {
 	private int m_dia;
 	private int m_mes;
 	private int m_anio;
-	private String m_hora;
-	
+	private int m_hora;
+	private int m_minutos;
+
 	private Calendar m_fecha;
+	private int m_segundos;
+
 	public Fecha()
 	{
-		m_fecha = new GregorianCalendar();
+		m_fecha = Calendar.getInstance();
 		configFecha();
 	}
+
 	private void configFecha()
 	{
 		m_dia = m_fecha.get(Calendar.DAY_OF_MONTH);
-		m_mes = m_fecha.get(Calendar.MONTH)+1;
+		m_mes = m_fecha.get(Calendar.MONTH) + 1;
 		m_anio = m_fecha.get(Calendar.YEAR);
-		m_hora = m_fecha.getTime().toString();
+		m_hora = m_fecha.get(Calendar.HOUR_OF_DAY);
+		m_minutos = m_fecha.get(Calendar.HOUR_OF_DAY);
+		m_segundos = m_fecha.get(Calendar.SECOND);
 	}
-	
+
 	@Override
 	public String toString()
 	{
-		return m_dia+"/"+m_mes+"/"+m_anio+"-"+m_hora;
+		return m_dia + "/" + m_mes + "/" + m_anio + "---" + m_hora + ":" + m_minutos +":"+m_segundos;
 	}
 }
