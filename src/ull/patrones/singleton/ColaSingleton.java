5d33962cb2e3d76d22a61ffd90637f77e44c5c29
Orlandy Ariel <alu0100773408@ull.edu.es>
2016-11-03 15:18:36 +0000

    Comentarios en la cola

--- src/ull/patrones/singleton/ColaSingleton.java
+++ src/ull/patrones/singleton/ColaSingleton.java
@@ -3,33 +3,51 @@ package ull.patrones.singleton;
 import java.util.LinkedList;
 
 import ull.patrones.estrategia.IEvento;
-
+/**
+ * @author Orlandy Ariel Sánchez A.
+ *	Clase ColaSingleton que representa la lista de eventos.
+ */
 public class ColaSingleton
 {
 	private static ColaSingleton m_instancia = new ColaSingleton();
 	private static LinkedList<IEvento> m_cola;
-
+	/**
+	 * Constructor por defecto privado, de manera que no se puedan crear nuevos objetos de
+	 * esta clase fuera.
+	 */
 	private ColaSingleton()
 	{
 		m_cola = new LinkedList<>();
 	}
-
+	/**
+	 * Método para obtener la instancia de esta clase
+	 * @return, devuelve la instancia de la clase
+	 */
 	public static ColaSingleton getInstancia()
 	{
 		return m_instancia;
 	}
-
+	/**
+	 * Método para añadir un elemento a la cola
+	 * @param a_evento, objeto de tipo IEvento para almacenarlo en la cola
+	 */
 	public void acolar(IEvento a_evento)
 	{
 		m_cola.addFirst(a_evento);
 		m_cola.getFirst().start();
 	}
-
-	public IEvento desacolar()
+	/**
+	 * Método que elimina el último elemento de la cola
+	 * 
+	 */
+	public void desacolar()
 	{
-		return m_cola.removeLast();
+		m_cola.removeLast();
 	}
-
+	/**
+	 * Método para eliminar un objeto concreto de la cola
+	 * @param a_evento, elemento que se quiere borrar
+	 */
 	public void desacolar(IEvento a_evento)
 	{
 		m_cola.remove((Object) a_evento);
