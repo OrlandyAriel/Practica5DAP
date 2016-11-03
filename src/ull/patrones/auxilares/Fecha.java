12defb6d0460e5fdc7b7cacc0348a14ae0073e45
Orlandy Ariel <alu0100773408@ull.edu.es>
2016-11-03 16:58:55 +0000

    getters de los atributos de la clase Fecha

--- src/ull/patrones/auxilares/Fecha.java
+++ src/ull/patrones/auxilares/Fecha.java
@@ -1,7 +1,10 @@
 package ull.patrones.auxilares;
 
 import java.util.Calendar;
-
+/**
+ * @author Orlandy Ariel Sánchez A.
+ *	clase para obtener la fecha
+ */
 public class Fecha
 {
 	private int m_dia;
@@ -12,13 +15,17 @@ public class Fecha
 
 	private Calendar m_fecha;
 	private int m_segundos;
-
+	/**
+	 * Constructor por defecto que obtiene la fecha del sistema
+	 */
 	public Fecha()
 	{
 		m_fecha = Calendar.getInstance();
 		configFecha();
 	}
-
+	/**
+	 * Método que configura la fecha con su dia y hora
+	 */
 	private void configFecha()
 	{
 		m_dia = m_fecha.get(Calendar.DAY_OF_MONTH);
@@ -27,6 +34,36 @@ public class Fecha
 		m_hora = m_fecha.get(Calendar.HOUR_OF_DAY);
 		m_minutos = m_fecha.get(Calendar.HOUR_OF_DAY);
 		m_segundos = m_fecha.get(Calendar.SECOND);
+	}
+
+	public int getM_dia()
+	{
+		return m_dia;
+	}
+
+	public int getM_mes()
+	{
+		return m_mes;
+	}
+
+	public int getM_anio()
+	{
+		return m_anio;
+	}
+
+	public int getM_hora()
+	{
+		return m_hora;
+	}
+
+	public int getM_minutos()
+	{
+		return m_minutos;
+	}
+
+	public int getM_segundos()
+	{
+		return m_segundos;
 	}
 
 	@Override
